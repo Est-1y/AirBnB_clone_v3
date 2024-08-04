6cf71ec232d7e0a98e21cf31138a7b9ffa697c05
Est-1y <133119207+Est-1y@users.noreply.github.com>
2024-08-04 10:36:14 +0300

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-""" holds class City"""
+"""class City"""
 import models
 from models.base_model import BaseModel, Base
 from os import getenv
@@ -9,16 +9,16 @@ from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
-    """Representation of city """
+    """Representing city """
     if models.storage_t == "db":
         __tablename__ = 'cities'
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
         name = Column(String(128), nullable=False)
-        places = relationship("Place", backref="cities")
+        places = relationship("Place", backref="cities", cascade="all, delete")
     else:
         state_id = ""
         name = ""
 
     def __init__(self, *args, **kwargs):
-        """initializes city"""
+        """initializing city."""
         super().__init__(*args, **kwargs)
